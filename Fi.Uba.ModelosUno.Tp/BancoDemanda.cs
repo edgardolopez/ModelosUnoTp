24f638c77f731cbaf6910621bddd2ea66311a347
Edgar Lopez <Edgar.Lopez>
2022-04-23 05:14:11 -0300

    load txt file

---
+++ Fi.Uba.ModelosUno.Tp/BancoDemanda.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fi.Uba.ModelosUno.Tp
+{
+    public class BancoDemanda
+    {
+        public string Nombre { get; set; }
+        public int Monto { get; set; }
+
+        public double CoordenadaX { get; set; }
+        public double CoordenadaY { get; set; }
+
+
+        public double Distancia(BancoDemanda bancoDestino) {
+            //La distancia entre las sucursales i y j es sqrt((Xi-Xj)^2+(Yi-Yj)^2)
+            var inicial = Math.Pow(this.CoordenadaX - bancoDestino.CoordenadaX, 2);
+            var final = Math.Pow(this.CoordenadaY - bancoDestino.CoordenadaY, 2);
+            return Math.Sqrt(inicial + final);
+        }
+
+    }
+}
--- Fi.Uba.ModelosUno.Tp/Program.cs
+++ Fi.Uba.ModelosUno.Tp/Program.cs
@@ -1,3 +1,32 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using Fi.Uba.ModelosUno.Tp;
+
+string[] lines = System.IO.File.ReadAllLines(@".\problema_uno.txt");
+var capacidadMaxima = Convert.ToDecimal(lines[0].Split(' ')[1]);
+var dimension = Convert.ToDecimal(lines[1].Split(' ')[1]);
+var posicionDemandasInicial = lines.ToList().IndexOf("DEMANDAS");
+var posicionDemandasFinal = lines.ToList().IndexOf("FIN DEMANDAS");
+var posicionCoordenadasInicial = lines.ToList().IndexOf("NODE_COORD_SECTION");
+var posicionCoordenadasFinal = lines.ToList().IndexOf("EOF");
+var coordenadas = new Dictionary<string, string>();
+
+for (int i = posicionCoordenadasInicial + 1; i < posicionCoordenadasFinal; i++)
+{
+    coordenadas.Add(lines[i].Split(' ')[0].Trim(), lines[i]);
+}
+
+var bancoDemandas = new List<BancoDemanda>();
+
+for (int i = posicionDemandasInicial + 1; i < posicionDemandasFinal; i++)
+{
+    var nombre = lines[i].Split(' ')[0].Trim();
+    var monto = Convert.ToInt32(lines[i].Split(' ')[1].Trim());
+
+    bancoDemandas.Add(new BancoDemanda()
+    {
+        Nombre = nombre,
+        Monto = monto,
+        CoordenadaX = Convert.ToDouble(coordenadas.FirstOrDefault(x => x.Key == nombre).Value.Split(' ')[1].Trim()),
+        CoordenadaY = Convert.ToDouble(coordenadas.FirstOrDefault(x => x.Key == nombre).Value.Split(' ')[2].Trim())
+    });
+}
 
